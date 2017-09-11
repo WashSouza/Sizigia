3775f7f78622bb823a04f26c6bb27d2fdc2543df
Washington Mário de Jesus Souza <tonmjs@gmail.com>
2017-09-10 22:01:16 -0300

    no message

---
+++ Sizígia/Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    
+    public Material skyboxNormal;
+    public Material skyboxReflection;
+
+    private Material skyboxCurrent;
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        print(RenderSettings.skybox);
+
+        if (Globals.currentDimension == "Normal")
+            skyboxCurrent = skyboxNormal;
+        else
+            skyboxCurrent = skyboxReflection;
+
+
+        RenderSettings.skybox = skyboxCurrent;
+    }
+}
---
+++ Sizígia/Assets/Mirror.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mirror : MonoBehaviour {
+
+    public Transform destination;
+    private GameObject player;
+
+    private bool playerColliding;
+
+    private GameObject InstText;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+        InstText = GameObject.Find("Canvas").transform.Find("MirrorText").gameObject;
+
+        playerColliding = false;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (playerColliding)
+            InstText.SetActive(true);
+        else
+            InstText.SetActive(false);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (playerColliding)
+            {
+                player.transform.position = destination.position;
+                ChangeDimension();
+            }
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject == player)
+            playerColliding = true;
+        else
+            playerColliding = false;
+        
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        playerColliding = false;
+    }
+
+    private void ChangeDimension()
+    {
+        if (Globals.currentDimension == "Normal")
+            Globals.currentDimension = "Reflection";
+        else
+            Globals.currentDimension = "Normal";
+
+    }
+
+}
